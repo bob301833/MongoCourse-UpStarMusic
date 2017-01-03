f0de15509994a66d616638c7c8cbf05c17cf8310
yuhsing <bob301833@gmail.com>
2017-01-03 12:18:58 +0800

    Indexs and Text Search

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -9,7 +9,7 @@ import Artist from '../models/artist';
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 export default (criteria, sortProperty, offset = 0, limit = 20) => {
-  const query = Artist.find({})
+  const query = Artist.find(buildQuery(criteria))
     .sort({ [sortProperty]: 1 })
     .skip(offset)
     .limit(limit);
@@ -23,4 +23,26 @@ export default (criteria, sortProperty, offset = 0, limit = 20) => {
         limit
       };
     });
+};
+
+const buildQuery = (criteria) => {
+  const query = {};
+  if (criteria.name) {
+    query.$text = { $search: criteria.name };
+  }
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    };
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    };
+  }
+  return query;
 };
